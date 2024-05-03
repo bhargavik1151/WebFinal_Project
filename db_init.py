c6b1392ea94ee18ec6ef93b8696d82d0a8b443b6
bhargavik1151 <161762431+bhargavik1151@users.noreply.github.com>
2024-05-02 23:24:52 -0400

    Update db_init.py
    Inserting data into tables using example_orders.json file

--- db_init.py
+++ db_init.py
@@ -51,3 +51,39 @@ curr.execute(
 """
 )
 
+with open('example_orders.json') as file:
+    order_list = json.load(file)
+
+orders = []
+customers = {}
+items = {}
+for order in order_list:
+    customers[order["phone"]] = order["name"]
+    for item in order["items"]:
+        items[item["name"]] = item["price"]
+for phone, name in customers.items():
+    curr.execute("INSERT INTO customers (name, phone) VALUES (?, ?);", (name, phone))
+
+curr.execute("SELECT * FROM customers;")
+print("Customers:")
+print(curr.fetchall())
+
+for name, price in items.items():
+    curr.execute("INSERT INTO items (name, price) VALUES (?, ?);", (name, price))
+
+for order in order_list:
+    curr.execute("SELECT id FROM customers WHERE phone=?;", (order["phone"],))
+    cust_id = curr.fetchone()[0]
+    curr.execute("INSERT INTO orders (notes, timestamp, cust_id) VALUES (?, ?, ?);",
+                 (order["notes"], order["timestamp"], cust_id))
+    order_id = curr.lastrowid
+    for item in order["items"]:
+        curr.execute("SELECT id FROM items WHERE name=?;", (item["name"],))
+        item_id = curr.fetchone()[0]
+        curr.execute("INSERT INTO order_list (order_id, item_id) VALUES (?, ?);", 
+                     (order_id, item_id))
+
+
+conn.commit()
+conn.close()
+
