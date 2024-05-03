5030301686e8bb3317954a0483064eb7bbf1408f
bhargavik1151 <161762431+bhargavik1151@users.noreply.github.com>
2024-05-02 23:21:22 -0400

    Update db_init.py
    customers table creation.

--- db_init.py
+++ db_init.py
@@ -2,3 +2,12 @@ import sqlite3
 import json
 conn = sqlite3.connect("db.sqlite")
 curr = conn.cursor()
+
+curr.execute("""
+             CREATE TABLE customers(
+             id INTEGER PRIMARY KEY,
+             name CHAR(64),
+             phone CHAR(10)
+             );
+             """
+             )
