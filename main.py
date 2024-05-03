46e64793310efab440457d6901a61ad40b0164e2
bhargavik1151 <161762431+bhargavik1151@users.noreply.github.com>
2024-05-02 23:27:11 -0400

    Update main.py
    Define models

--- main.py
+++ main.py
@@ -5,3 +5,19 @@ import time
 
 # Initialize FastAPI app
 app = FastAPI()
+# Define models
+class Customer(BaseModel):
+    cust_id: int | None = None
+    name: str
+    phone: str
+
+class Item(BaseModel):
+    id: int | None = None
+    name: str
+    price: float
+
+class Order(BaseModel):
+    order_id: int | None = None
+    notes: str
+    cust_id: int
+    timestamp: int
